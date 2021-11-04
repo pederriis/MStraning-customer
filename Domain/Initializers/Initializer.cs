47720e60d738aaab4a3aea9d37656e98874e29f2
pederriis <pederriis@gmail.com>
2021-11-04 11:28:50 +0100

    added seeder

---
+++ Domain/Initializers/Initializer.cs
@@ -0,0 +1,38 @@
+﻿using MStraning_customer.Persistence.Contexts;
+using MStraning_customer.Persistence.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MStraning_customer.Domain.Initializers
+{
+    public class Initializer
+    {
+
+
+        public static void SeedDatabase(CustomerContext customerContext)
+        {
+            //sikrer sig at databasen eksisterer og skaber, den hvis den ikke gør
+            customerContext.Database.EnsureCreated();
+
+            //seeder databasen, hvis den er tom
+            if (!customerContext.Customers.Any())
+            {
+
+                List<CustomerEF> customers = new List<CustomerEF> {
+                new CustomerEF { Name = "Hans", PhoneNumber="566" },
+                new CustomerEF { Name = "Ole", PhoneNumber="987"},
+
+            };
+
+                foreach (var c in customers)
+                {
+                    customerContext.Add(c);
+                }
+                customerContext.SaveChanges();
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MStraning_customer.Domain.Initializers;
+using MStraning_customer.Persistence.Contexts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +14,34 @@ namespace MStraning_customer
 {
     public class Program
     {
+        //public static void Main(string[] args)
+        //{
+        //    CreateHostBuilder(args).Build().Run();
+        //}
+
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+
+                try
+                {
+                    var context = services.GetRequiredService<CustomerContext>();
+                    Initializer.SeedDatabase(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +50,24 @@ namespace MStraning_customer
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<CustomerContext>();
+                    Initializer.SeedDatabase(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
     }
 }
